85be69f8d46433ba4e47ac4aca5419d2ffe03163
Ankit-cs <ankitcareer018@gmail.com>
2025-01-09 16:25:00 +0000

    Not able to oslve other quesrions because of osme assignments i have to submit in college

---
+++ 09-01-2025/Nuts_and_Bolts_Problem.cpp
@@ -0,0 +1,7 @@
+/*
+Most INTUTIVE: I just think by using constraints 
+
+*Create a helper function to perform binary search on the predefined order.
+*Use this binary search to map each nut to its corresponding bolt.
+*Sort both nuts and bolts arrays based on their positions in the predefined order.
+*/
